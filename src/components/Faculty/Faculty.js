aa73a9ff27e861af7b80af9ce14a8543711d8d98
Brajesh Anokha <brajesh.anokha@publicissapient.com>
2022-08-24 01:59:20 +0530

    user faculty screen

--- src/components/Faculty/Faculty.js
+++ src/components/Faculty/Faculty.js
@@ -3,12 +3,14 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import moment from "moment";
+// import Highlighter from "react-highlight-words";
 import { Modal, Table, Form, Input, Button, PageHeader, Spin, Card, Row, Col, Divider, Select, DatePicker, Upload, Switch, Progress, notification, InputNumber } from "antd";
 const { TextArea } = Input;
 const { Option } = Select;
 
 import {
   EditOutlined,
+  // SearchOutlined,
 } from "@ant-design/icons";
 
 import "./Faculty.css";
@@ -54,7 +56,8 @@ function Faculty() {
   const confirmEditFacultyUserLoadingState = useSelector((state) => state.user.confirmEditFacultyUserLoadingState);
   const selectedFacultyUserForEdit = useSelector((state) => state.user.selectedFacultyUserForEdit);
 
-  // console.log("facultyOrUsersList---------", facultyOrUsersList);
+  console.log("facultyOrUsersList---------", facultyOrUsersList);
+  console.log("dddd---------", facultyOrUsersList.map(fac => fac.firstName));
   
   const layout = {
     labelCol: {
@@ -151,12 +154,119 @@ function Faculty() {
     dispatch(setSelectedFacultyUserForEdit(undefined));
     dispatch(showHideModal(false));
   };
+  
+  // column search starts
+
+  // const [searchText, setSearchText] = useState("");
+  // const [searchedColumn, setSearchedColumn] = useState("");
+  // const searchInput = useRef(null);
+
+  // const handleSearch = (selectedKeys, confirm, dataIndex) => {
+  //   confirm();
+  //   setSearchText(selectedKeys[0]);
+  //   setSearchedColumn(dataIndex);
+  // };
+
+  // const handleReset = (clearFilters) => {
+  //   clearFilters();
+  //   setSearchText("");
+  // };
+
+  // const getColumnSearchProps = (dataIndex) => ({
+  //   filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+  //     <div
+  //       style={{
+  //         padding: 8,
+  //       }}
+  //     >
+  //       <Input
+  //         ref={searchInput}
+  //         placeholder={`Search ${dataIndex}`}
+  //         value={selectedKeys[0]}
+  //         onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+  //         onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+  //         style={{
+  //           marginBottom: 8,
+  //           display: "block",
+  //         }}
+  //       />
+  //       <Space>
+  //         <Button
+  //           type="primary"
+  //           onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+  //           icon={<SearchOutlined />}
+  //           size="small"
+  //           style={{
+  //             width: 90,
+  //           }}
+  //         >
+  //           Search
+  //         </Button>
+  //         <Button
+  //           onClick={() => clearFilters && handleReset(clearFilters)}
+  //           size="small"
+  //           style={{
+  //             width: 90,
+  //           }}
+  //         >
+  //           Reset
+  //         </Button>
+  //         <Button
+  //           type="link"
+  //           size="small"
+  //           onClick={() => {
+  //             confirm({
+  //               closeDropdown: false,
+  //             });
+  //             setSearchText(selectedKeys[0]);
+  //             setSearchedColumn(dataIndex);
+  //           }}
+  //         >
+  //           Filter
+  //         </Button>
+  //       </Space>
+  //     </div>
+  //   ),
+  //   filterIcon: (filtered) => (
+  //     <SearchOutlined
+  //       style={{
+  //         color: filtered ? "#1890ff" : undefined,
+  //       }}
+  //     />
+  //   ),
+  //   onFilter: (value, record) =>
+  //     record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+  //   onFilterDropdownVisibleChange: (visible) => {
+  //     if (visible) {
+  //       setTimeout(() => searchInput.current?.select(), 100);
+  //     }
+  //   },
+  //   render: (text) =>
+  //     searchedColumn === dataIndex ? (
+  //       <Highlighter
+  //         highlightStyle={{
+  //           backgroundColor: "#ffc069",
+  //           padding: 0,
+  //         }}
+  //         searchWords={[searchText]}
+  //         autoEscape
+  //         textToHighlight={text ? text.toString() : ""}
+  //       />
+  //     ) : (
+  //       text
+  //     ),
+  // });
+
+  // column search ends
 
   const facultyOrUsersColumn = [
     {
       title: "First Name",
       dataIndex: "firstName",
-      width: "10%",
+      width: "15%",
+      filters: facultyOrUsersList.map(fac => { return {text: fac.firstName, value: fac.firstName} }), 
+      onFilter: (value, record) => record.firstName.indexOf(value) === 0,
+      // ...getColumnSearchProps("firstName"),
     },
     {
       title: "Middle Name",
